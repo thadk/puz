ebfa758d5a150a1e44bc8f696fb01fe3e9286953
Thad Kerosky <contact@thadk.net>
2014-08-07 17:53:36 -0400

    when click on <p>, retrieve data-uid from parent node, replace text

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ $( "form#geoname" ).on( "submit", function( event ) {
   var formContents = $( this ).serialize();
   var ajaxGeoname;
 
-  console.log(formContents);
+  /* console.log(formContents); */
+
   if (formContents !== undefined) {
     var iterateResults = function (index, value) {
       /* a little scary for XSS */
@@ -15,17 +16,21 @@ $( "form#geoname" ).on( "submit", function( event ) {
     };
 
     ajaxGeoname = $.ajax("http://api.geonames.org/search?formatted=true&lang=en&username=aiddata&fuzzy=0&maxRows=5&startRow=0&orderby=relevance&type=json&"+formContents);
-
     $.when(ajaxGeoname).then( function(result) {
       var topThreePlaces = result.geonames.slice(0,3);
       $("#left-sidebar #results").text("");
       $.each(topThreePlaces, iterateResults);
 
+    }).fail( function (result) {
+     $("#left-sidebar #results").html("<li>Could not contact API</li>");
     }).done();
-
 
   }
 
+});
+
+$("div#main-container > div > p").click(function (event) {
+  $(this).text($(this.parentNode).find("img").attr('data-uid'));
 });
 
 }());
