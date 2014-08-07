0e62aef397127addda5650c1d14031e17e08bd29
Thad Kerosky <contact@thadk.net>
2014-08-07 17:26:30 -0400

    feed serialized form through to API; slice 0,3

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 (function() {
 
 
-$( "form" ).on( "submit", function( event ) {
+$( "form#geoname" ).on( "submit", function( event ) {
   event.preventDefault();
-  console.log( $( this ).serialize() );
+  var formContents = $( this ).serialize();
+  var ajaxGeoname;
+
+  console.log(formContents);
+  if (formContents !== undefined) {
+    ajaxGeoname = $.ajax("http://api.geonames.org/search?formatted=true&lang=en&username=aiddata&fuzzy=0&maxRows=5&startRow=0&orderby=relevance&type=json&"+formContents);
+
+    $.when(ajaxGeoname).then( function(result) {
+      console.log(result.geonames.slice(0,3));
+    }).done();
+
+  }
+
 });
 
 }());
